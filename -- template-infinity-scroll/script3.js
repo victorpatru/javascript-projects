638b6a3cbdbe8933400133ea4f78f42f25fc40ce
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-28 16:47:42 +0300

    spec third/final pass

---
+++ -- template-infinity-scroll/script3.js
@@ -0,0 +1,7 @@
+// 1. Set global variables
+// 2. imageLoaded() functionality
+// 3. Helper function for setting multiple attributes at once
+// 3. Create elements (links/photos) and put them in the DOM (displayPhotos() function)
+// 4. Fetch data from the Unsplash API (getPhotos() function)
+// 5. Track whether we are close to running out of images while scrolling. If yes fetch more data from the Unsplash API
+// 6. Fetch images on load
