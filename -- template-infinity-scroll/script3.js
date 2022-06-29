7af49b4db2fec4b26eeaf4df9ee0d65766259eb8
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-29 15:24:17 +0300

    complete third pass

--- -- template-infinity-scroll/script3.js
+++ -- template-infinity-scroll/script3.js
@@ -5,3 +5,72 @@
 // 4. Fetch data from the Unsplash API (getPhotos() function)
 // 5. Track whether we are close to running out of images while scrolling. If yes fetch more data from the Unsplash API
 // 6. Fetch images on load
+
+const imageContainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
+
+const count = 30
+const apiKey = ''
+
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
+let imagesArray = []
+
+function imageLoaded() {
+    imagesLoaded++
+
+    if (imagesLoaded === totalImages) {
+        ready = true
+        loader.hidden = true
+    }
+}
+
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+function displayPhotos() {
+    imagesLoaded = 0
+    totalImages = imagesArray.length
+
+    imagesArray.forEach(photo => {
+        const item = document.createElement('a')
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
+
+        const img = document.createElement('img')
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+
+        img.addEventListener('load', imageLoaded)
+
+        item.appendChild(img)
+        imageContainer.appendChild(item)
+    })
+}
+
+async function getPhotos() {
+    try {
+        const response = await fetch('')
+        imagesArray = await response.json()
+    } catch(error) {
+        // Throw error
+    }
+}
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false
+        getPhotos()
+    }
+})
+
+getPhotos()
