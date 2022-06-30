aab44dd51ff93a645c887dd5d3b7ee2da52e608f
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-30 11:13:27 +0300

    second pass picture in picture

--- picture-in-picture/script2.js
+++ picture-in-picture/script2.js
@@ -1,3 +1,28 @@
 // 1. Get HTML elements
 // 2. Get media stream, pass to video, and play
 // 3. When you click the button trigger the Picture-in-Picture functionality
+
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function getStream() {
+    try {
+        video.srcObject = await navigator.mediaDevices.getDisplayMedia();
+        video.onloadedmetadata = () => video.play();
+        
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+
+
+button.addEventListener('click', async () => {
+   button.disabled = true;
+
+   await video.requestPictureInPicture();
+
+   button.disabled = false;
+});
+
+getStream()
