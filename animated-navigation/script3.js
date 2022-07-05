b80e1a3e3c175008aeadb6c73f96347f75aae12c
Victor Patru <patruvictor.mihai@gmail.com>
2022-07-05 13:04:40 +0300

    third pass animated-navigation

---
+++ animated-navigation/script3.js
@@ -0,0 +1,36 @@
+// Select elements
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+
+// Place in an array for easier navigation
+navElements = [nav1, nav2, nav3, nav4, nav5];
+
+// Create a function that iterates over our nav items and replaces the direction of the slide (out/in)
+function navAnimation(direction1, direction2) {
+    navElements.forEach((nav, i) => nav.classList.replace(`slide-${direction1}-${i+1}`, `slide-${direction2}-${i+1}`));
+}
+
+
+// It should "change" our menuBar class, and overlay (using the navAnimation). Use the overlay active trick to track whether the button has been clicked or not
+function toggleNav() {
+   menuBars.classList.toggle('change');
+
+   overlay.classList.toggle('overlay-active');
+
+   if (overlay.classList.contains('overlay-active')) {
+    navAnimation('out', 'in');
+    overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+   } else {
+    navAnimation('in', 'out');
+    overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+   }
+}
+
+// Add event listeners
+menuBars.addEventListener('click', toggleNav);
+navElements.forEach(nav => nav.addEventListener('click', toggleNav));
